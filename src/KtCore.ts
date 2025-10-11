d3c38f3f10aca3bd1cf763f51fbb3d1db4e9c30a
octopodo <orson_wells40@hotmail.com>
2025-10-11 12:47:02 +0200

    refactor: rename KT class to __KT_Core and update exports in index.ts

--- src/KtCore.ts
+++ src/KtCore.ts
@@ -1,19 +1,19 @@
 import { KT_Paterns } from "./patterns";
 
-export class KT {
+class __KT_Core {
     private name = "KtCore";
     private version = "1.0.0";
-    public static patterns = KT_Paterns;
-    static salute() {
+    public patterns = KT_Paterns;
+    salute() {
         const obj = { name: "KtCore", version: "1.0.0" };
         alert(JSON.stringify(obj));
         alert(`Hello from ${this.name} `);
     }
 
-    static init() {
+    init() {
         return this.name;
     }
-    static Module(name: string, module: any) {
+    Module(name: string, module: any) {
         if ((this as any)[name]) {
             $.writeln(`Module ${name} already exists`);
             return;
@@ -21,3 +21,5 @@ export class KT {
         (this as any)[name] = module;
     }
 }
+
+export const KT_Core = new __KT_Core();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 //@include "lib/json2.js"
 
-import { KT } from "./KtCore";
+import { KT_Core } from "./KtCore";
 import { KT_Paterns } from "./patterns";
-KT.init();
-export { KT, KT_Paterns };
+
+export { KT_Core, KT_Paterns };
