5d0fbb0ad95db265a86df7bcccea00a32b5ea0b2
octopodo <orson_wells40@hotmail.com>
2025-11-22 23:09:56 +0100

    chore: bump version to 1.1.12 in package.json; implement Array.prototype.includes method in es-shim

--- src/lib/es-shim.js
+++ src/lib/es-shim.js
@@ -66,6 +66,27 @@ if (!Array.prototype.map) {
         return result;
     };
 }
+if (!Array.prototype.includes) {
+    Array.prototype.includes = function (searchElement, fromIndex) {
+        if (this == null) {
+            throw new TypeError('"this" is null or not defined');
+        }
+        var o = Object(this);
+        var len = o.length >>> 0;
+        if (len === 0) {
+            return false;
+        }
+        var n = fromIndex | 0;
+        var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+        while (k < len) {
+            if (o[k] === searchElement) {
+                return true;
+            }
+            k++;
+        }
+        return false;
+    };
+}
 
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (searchElement, fromIndex) {
